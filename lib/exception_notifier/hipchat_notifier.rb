11ecde01b788a0c0d9824ccb7d06a134bf5e653f
Aaron Venezia <aaronvenezia@weber.edu>
2015-02-18 11:02:27 -0700

    Tailoring message to show user and company in exception message

--- lib/exception_notifier/hipchat_notifier.rb
+++ lib/exception_notifier/hipchat_notifier.rb
@@ -10,13 +10,19 @@ module ExceptionNotifier
         api_token         = options.delete(:api_token)
         room_name         = options.delete(:room_name)
         opts              = {
-                              :api_version => options.delete(:api_version) || 'v1',
-                              :server_url => options.delete(:server_url) || nil
-                            }
+            :api_version => options.delete(:api_version) || 'v1',
+            :server_url => options.delete(:server_url) || nil
+        }
         @from             = options.delete(:from) || 'Exception'
         @room             = HipChat::Client.new(api_token, opts)[room_name]
-        @message_template = options.delete(:message_template) || ->(exception) {
-          "Exception: '#{exception.message.split('<').join('').split('>').join('')}' on '#{exception.backtrace.first}'"
+        @message_template = options.delete(:message_template) || ->(exception, the_options) {
+          puts the_options[:env]['exception_notifier.exception_data']
+          if the_options
+            data = the_options[:env]['exception_notifier.exception_data']
+            "User: #{data[:user]} from #{data[:company]} Getting Exception: '#{exception.message.split('<').join('').split('>').join('')}' on '#{exception.backtrace.first}'"
+          else
+            "Exception: '#{exception.message.split('<').join('').split('>').join('')}' on '#{exception.backtrace.first}'"
+          end
         }
         @message_options  = options
         @message_options[:color] ||= 'red'
@@ -28,7 +34,7 @@ module ExceptionNotifier
     def call(exception, options={})
       return if !active?
 
-      message = @message_template.call(exception)
+      message = @message_template.call(exception, options)
       @room.send(@from, message, @message_options)
     end
 
